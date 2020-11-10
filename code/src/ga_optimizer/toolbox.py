3f612cb9877d85198a35398cb963f4771daa2786
lulivi <luislivilla@gmail.com>
2020-11-11 00:17:06 +0100

    Specifies learning rate for doc and code
    Closes #9

--- code/src/ga_optimizer/toolbox.py
+++ code/src/ga_optimizer/toolbox.py
@@ -91,7 +91,7 @@ def individual_evaluator(
         model.layers[layer_index].set_weights([layer.weights, layer.bias])
 
     model.compile(
-        optimizer=SGD(),
+        optimizer=SGD(learning_rate=0.01),
         loss=CategoricalCrossentropy()
         if multi_class
         else BinaryCrossentropy(),
