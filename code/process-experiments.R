66c77fe7070fa41b27996d097f96c0520598a63a
JJ Merelo <jjmerelo@gmail.com>
2020-11-22 19:38:41 +0100

    Processing all data

--- code/process-experiments.R
+++ code/process-experiments.R
@@ -1,10 +1,15 @@
 require(ggplot2)
 
-for (val in c("helicases","cancer1", "cancer2", "cancer3", "spambase1" ) ) {
+for (val in c("helicases","cancer1", "cancer2", "cancer3", "spambase1", "spambase2", "spambase3" ) ) {
     data <- read.csv(paste0("../data/",val,".csv"))
     print(val)
     print("Validation")
     print(summary(data$Validation))
+    print(sd(data$Validation))
     print("Test")
     print(summary(data$Test))
+    print(sd(data$Test))
+    print("Generations")
+    print(summary(data$Generations))
+    print(sd(data$Generations))
 }
