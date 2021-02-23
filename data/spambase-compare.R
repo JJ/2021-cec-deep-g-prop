4f1a480aa909cc6402c04edfcac1fb350d2c44ab
JJ Merelo <jjmerelo@gmail.com>
2021-02-23 17:57:37 +0100

    Script chart for comparison

--- data/spambase-compare.R
+++ data/spambase-compare.R
@@ -1,4 +1,5 @@
-
+library(ggplot2)
+library(ggthemes)
 
 spambase1.base <- read.csv("../data/spambase1-p64.csv")
 spambase2.base <- read.csv("../data/spambase2-p64.csv")
@@ -28,3 +29,14 @@ wilcox.test( spambase3.lr11$Validation, spambase3.tf$Validation)
 wilcox.test( spambase1.lr11$Test, spambase1.tf$Test)
 wilcox.test( spambase2.lr11$Test, spambase2.tf$Test)
 wilcox.test( spambase3.lr11$Test, spambase3.tf$Test)
+
+comparison.layers <- data.frame(experiment=c(rep("spambase1.3L",length(spambase1.tf$Test)),
+                                             rep("spambase1.1L",length(spambase1.lr11$Test)),
+                                             rep("spambase2.3L",length(spambase2.tf$Test)),
+                                             rep("spambase2.1L",length(spambase2.lr11$Test)),
+                                             rep("spambase3.3L",length(spambase3.tf$Test)),
+                                             rep("spambase3.1L",length(spambase3.lr11$Test))),
+                                Accuracy=c(spambase1.tf$Test,spambase1.lr11$Test,
+                                           spambase2.tf$Test,spambase2.lr11$Test,
+                                           spambase3.tf$Test,spambase3.lr11$Test))
+ggplot(comparison.layers,aes(x=experiment,y=Accuracy,group=experiment))+geom_boxplot(notch=T)+theme_tufte()
