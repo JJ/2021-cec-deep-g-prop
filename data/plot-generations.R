13564a4d26a78b27e6dde9d8e559749ca3d71e8c
JJ Merelo <jjmerelo@gmail.com>
2021-02-23 10:16:17 +0100

    Chart inserted in the paper, closes #47

--- data/plot-generations.R
+++ data/plot-generations.R
@@ -3,3 +3,6 @@ library(ggthemes)
 
 generations <- read.csv("../data/ng-spambase1-generations.csv")
 ggplot(generations,aes(x=Generation,y=Average.F2,group=Generation,color="Average F2"))+geom_boxplot()+geom_boxplot(aes(x=Generation,y=Max.F2,group=Generation, color="Max F2", fill="Max F2"))+theme_tufte()
+
+
+ggplot(generations,aes(x=Generation,y=Average.Acc,group=Generation,color="Average Accuracy"))+geom_boxplot()+geom_boxplot(aes(x=Generation,y=Min.Acc,group=Generation, color="Min Accuracy", fill="Max Accuracy"))+theme_tufte()
