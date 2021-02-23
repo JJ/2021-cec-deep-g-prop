6e7eb71b168657d0c6e3d66e4f9f20b0b6c3ff39
JJ Merelo <jjmerelo@gmail.com>
2021-02-23 08:30:26 +0100

    So it had not finished yet...

--- data/spambase-compare.R
+++ data/spambase-compare.R
@@ -8,10 +8,23 @@ spambase1.tf <- read.csv("../data/cec-2021-spambase-1.csv")
 spambase2.tf <- read.csv("../data/cec-2021-spambase-2.csv")
 spambase3.tf <- read.csv("../data/cec-2021-spambase-3.csv")
 
- wilcox.test( spambase1.tf$Validation, spambase1.base$Validation)
- wilcox.test( spambase2.tf$Validation, spambase2.base$Validation)
- wilcox.test( spambase3.tf$Validation, spambase3.base$Validation)
+wilcox.test( spambase1.tf$Validation, spambase1.base$Validation)
+wilcox.test( spambase2.tf$Validation, spambase2.base$Validation)
+wilcox.test( spambase3.tf$Validation, spambase3.base$Validation)
 
- wilcox.test( spambase1.tf$Test, spambase1.base$Test)
- wilcox.test( spambase2.tf$Test, spambase2.base$Test)
- wilcox.test( spambase3.tf$Test, spambase3.base$Test)
+wilcox.test( spambase1.tf$Test, spambase1.base$Test)
+wilcox.test( spambase2.tf$Test, spambase2.base$Test)
+wilcox.test( spambase3.tf$Test, spambase3.base$Test)
+
+spambase1.lr11 <- read.csv("../data/cec-2021-spambase1-lr11.csv")
+spambase2.lr11 <- read.csv("../data/cec-2021-spambase2-lr11.csv")
+spambase3.lr11 <- read.csv("../data/cec-2021-spambase3-lr11.csv")
+
+
+wilcox.test( spambase1.lr11$Validation, spambase1.base$Validation)
+wilcox.test( spambase2.lr11$Validation, spambase2.base$Validation)
+wilcox.test( spambase3.lr11$Validation, spambase3.base$Validation)
+
+wilcox.test( spambase1.lr11$Test, spambase1.base$Test)
+wilcox.test( spambase2.lr11$Test, spambase2.base$Test)
+wilcox.test( spambase3.lr11$Test, spambase3.base$Test)
