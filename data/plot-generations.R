b701e0fc836897ddf6e8678db87571e1751a4fb2
JJ Merelo <jjmerelo@gmail.com>
2021-02-23 09:46:51 +0100

    Plotting f2 evolution

---
+++ data/plot-generations.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+library(ggthemes)
+
+generations <- read.csv("../data/ng-spambase1-generations.csv")
+ggplot(generations,aes(x=Generation,y=Average.F2,group=Generation,color="Average F2"))+geom_boxplot()+geom_boxplot(aes(x=Generation,y=Max.F2,group=Generation, color="Max F2", fill="Max F2"))+theme_tufte()
